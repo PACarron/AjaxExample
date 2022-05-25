b8c206792f011d355320f9609b481a8e8d63c575
Pierre-Alexis Carron <pacarron@unis.fr>
2022-05-25 10:06:23 +0200

    Setup project

---
+++ src/main/java/com/example/ajax_java_list/AjaxJavaListApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajax_java_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxJavaListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxJavaListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ajax_java_list/AjaxJavaListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajax_java_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxJavaListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
